ad7770168b7df20f8e62dea4c91cbef2195f42d2
hugosenari <hugosenari@gmail.com>
2022-03-17 18:29:10 -0300

    chore: re-index ../nimpkgs/t/tkrzw/v0_1_0

--- v0_1_0/flake.nix
+++ v0_1_0/flake.nix
@@ -11,7 +11,7 @@
   inputs.src-tkrzw-v0_1_0.owner = "~ehmry";
   inputs.src-tkrzw-v0_1_0.ref   = "refs/tags/v0.1.0";
   inputs.src-tkrzw-v0_1_0.repo  = "nim-tkrzw";
-  inputs.src-tkrzw-v0_1_0.type  = "other";
+  inputs.src-tkrzw-v0_1_0.type  = "sourcehut";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
   let 
